91c934f79fbe1a117e40c1f81ef33a6ba79fd24f
idanvaxman1 <idanvaxman@icloud.com>
2026-07-07 09:12:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from modules.system_info import get_system_info
+
+
+get_system_info()
---
+++ modules/system_info.py
@@ -0,0 +1,8 @@
+import platform
+import os
+import socket
+import subprocess
+
+
+def get_system_info():
+    print("OS: ",platform.system())
